f800d4827f06229af655211b9b07667358e93106
zaki <hrzak39@gmail.com>
2023-06-09 00:08:02 +0100

    V1 commited

---
+++ .tinkerun/inspiring.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use app\Models;
+use App\Models\User;
+
+Inspiring::quote();
+
+
+// User::where('role','client')->orderBy('created_at')->limit(3)->get();
